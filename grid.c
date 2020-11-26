bed7e622a7cda03505e5874dca28b2d3ca0aac51
Max Zabarka <maxim.zabarka@gmail.com>
2020-11-26 11:54:32 -0700

    stuff

--- grid.c
+++ grid.c
@@ -9,12 +9,12 @@
     createArray(rows,columns,q);
 */
 
-void createArray(int rows, int columns, int (*grid)[rows]) {
+void createArray(int rows, int columns, char (*grid)[rows]) {
     for (int col=0; col<columns;col++){
         for(int row=0;row<rows;row++) {
             grid[row][col] = rand() % 2;
-            printf("%d",grid[row][col]);
+          //  printf("%d",grid[row][col]);
         }
-        printf("\n");
+        //printf("\n");
     }
 }
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <ncurses.h>
 #include <string.h>
+#include "grid.c"
 int main() {
 	//Initialize
 	initscr();
@@ -12,11 +13,35 @@ int main() {
 
 	//Create window and draw box
 	int margin = 1;
-	WINDOW* inputwin = newwin(yMax-margin,xMax-margin,margin,margin);
-	box(inputwin, 0, 0);
+	//WINDOW* gamewin = newwin(yMax-margin,xMax-margin,margin,margin);
+	WINDOW* gamewin = newwin(15,15,margin,margin);
+
+	box(gamewin, 0, 0);
 	int boxY,boxX;
-	getmaxyx(inputwin, boxY, boxX);
+	getmaxyx(gamewin, boxY, boxX);
 	printw("%d x %d\n",boxX,boxY);
+
+	//Draw grid
+	int gridX = 500;
+	int gridY = 500;
+
+	srand(time(NULL));
+    char grid[gridX][gridY];
+    char (*q)[gridX] = grid;
+    createArray(gridX,gridY,q);
+
+	for (int i=0;i<gridX;i++) {
+
+		for (int j=0;j<gridY;j++) {
+
+			if (!(grid[i][j] == 0)) {
+				mvwaddch(gamewin,j+1,i+1,'#');
+				
+			}
+		
+
+		}
+	}
 
 
 	//Print title
@@ -26,9 +51,12 @@ int main() {
 	mvprintw(0,xMax/2-titleLength/2,title);
 	
 	refresh();
-	wrefresh(inputwin);
+	wrefresh(gamewin);
 
 	getch();
 	endwin();
 	return 0;
+}
+void draw() {
+	
 }
