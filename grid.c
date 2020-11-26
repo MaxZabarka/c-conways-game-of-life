527feaa92d1327af333a63a52aedeb68f1a65d5f
Max Zabarka <maxim.zabarka@gmail.com>
2020-11-26 10:30:20 -0700

    Some stuff

--- grid.c
+++ grid.c
@@ -1,21 +1,52 @@
 #include <stdio.h>
-int main() {
-    int xSize, ySize;
-    xSize = 3;
-    ySize = 3;
-    int grid[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    int (*grid_pointer)[xSize] = grid;
+#include <stdlib.h>
+#include <time.h>
+// int **createArray(int rows,int columns) {
+//     int xSize, ySize;
+//     xSize = rows;
+//     ySize = columns;
+//     int grid[xSize][ySize];
+//     int (*grid_pointer)[xSize] = grid;
+//     srand(time(NULL));
+
+//     for (int col = 0; col<ySize;col++){
+//         printf("[");
+//         for(int row=0;row<xSize;row++) {
+//             **grid_pointer = rand() % 2;
+
+//             printf("%d, ",**grid_pointer);
+//             *(*grid_pointer)+=1;
+//         }
+//         printf("]\n");
+//         *(*grid_pointer)-=3;
+//         *(grid_pointer+=1);
+//     }
+//     return grid;
+// }
+void createArray(int rows, int columns,int grid[3][3]) {
+    grid[0][0] = 10;
+    // for (int col=0; col<columns;col++){
+    //     for(int row=0;row<rows;row++) {
+    //         grid[row][col] = rand() % 2;
+    //         printf("%d",grid[row][col]);
+    //     }
+    //     printf("\n");
+    // }
+}
+void main() {
+    int rows = 3;
+    int columns = 3;
     
-    for (int col = 0; col<ySize;col++){
-        printf("[");
-        for(int row=0;row<xSize;row++) {
-            printf("%d, ",**grid_pointer);
-            *(*grid_pointer)+=1;
-        }
-        printf("]\n");
+    int grid[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int (*q)[][3] = grid;
 
+    printf("%d\n",**q);
+    // grid[0][0] = 4;
+    // printf("%d\n",grid[0][0]);
+    // createArray(3,3,grid);
+    // printf("%d\n",grid[0][0]);
 
-        *(*grid_pointer)-=3;
-        *(grid_pointer+=1);
-    }
+    // srand(time(NULL));
+
+   // printf("%d",grid[0][0]);
 }
